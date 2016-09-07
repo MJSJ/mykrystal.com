4ceb9dd5d3c6328baad23883eb8205aa2bad35d5
zhang.yufeng <420866750@qq.com>
2016-09-07 17:05:10 +0800

    wx

--- app/modules/wx.py
+++ app/modules/wx.py
@@ -14,7 +14,7 @@ class CheckHandler(wx):
     def get(self):
         code = self.get_argument('code', '')
         at = self.get_access_token(code)
-        l.info(self.get_web_user(at))
+        # l.info(self.get_web_user(at))
     '''
     yf: 认证公众号
     '''
