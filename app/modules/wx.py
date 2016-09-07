47a22041410bd0e881d6787e3d3d8fd270d71470
zhang.yufeng <420866750@qq.com>
2016-09-07 18:10:13 +0800

    wx

--- app/modules/wx.py
+++ app/modules/wx.py
@@ -18,25 +18,25 @@ class CheckHandler(wx):
         at = self.get_access_token(code)
         user = self.get_web_user(at)
         l.info(json.dumps(user))
-        # ud = self.db.client(openid=user['openid'], unionid=user['unionid']).one()
-        # if ud:
-        #     pass
-        # else:
-        #     data = {
-        #         "openid": str(user['openid']),
-        #         "unionid": str(user['unionid']),
-        #         "nickname": str(user['nickname']),
-        #         "sex": str(user['sex']),
-        #         "province": str(user['province']),
-        #         "city": str(user['city']),
-        #         "country": str(user['country']),
-        #         "headimgurl": str(user['headimgurl'])
-        #     }
-        #     newu = self.db.client.add(**user)
-        # if tp == 0:
-        #     self.redirect(path)
-        # elif tp == 1:
-        #     self.render(path+'/index.html')
+        ud = self.db.client(openid=user['openid'], unionid=user['unionid']).one()
+        if ud:
+            pass
+        else:
+            data = {
+                "openid": str(user['openid']),
+                "unionid": str(user['unionid']),
+                "nickname": str(user['nickname']),
+                "sex": str(user['sex']),
+                "province": str(user['province']),
+                "city": str(user['city']),
+                "country": str(user['country']),
+                "headimgurl": str(user['headimgurl'])
+            }
+            newu = self.db.client.add(**json.dumps(user))
+        if tp == 0:
+            self.redirect(path)
+        elif tp == 1:
+            self.render(path+'/index.html')
     '''
     yf: 认证公众号
     '''
