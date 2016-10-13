bbc9e3efec4dc18226e12c4728aea390a27cd82f
zhang.yufeng <420866750@qq.com>
2016-10-13 10:39:47 +0800

    yf

--- app/modules/__init__.py
+++ app/modules/__init__.py
@@ -80,7 +80,7 @@ class base(tornado.web.RequestHandler):
     '''
     def get_access_token(self, c):
         # if hasattr(self.application, "access_token") == False:
-        url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx2dbca94de092ab7f&secret=e6d9aa897cbfb02fa06a25e5b69edc9f&code="+c+"&grant_type=authorization_code"
+        url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=wxba02de7bf9017b73&secret=c60e32b863533c050a5788c5426c84fb&code="+c+"&grant_type=authorization_code"
         req = urllib2.Request(url)
         res_data = urllib2.urlopen(req)
         res = res_data.read()
